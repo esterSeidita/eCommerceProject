5bc78808825ae3bb937ac0f920d7bda7a42750ee
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-04-07 13:41:50 +0200

    feat: login function

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -181,6 +181,12 @@ const checkoutGenerator = () => {
     }));
 
     q(".confirmPurchase").addEventListener("click", () => {
+        const localUserData = JSON.parse(localStorage.getItem("loginData"));
+        if(localUserData === null){
+            alert("Please login first :)");
+            loginGenerator();
+        }else{
+
         q(".checkout").innerHTML = `
         <form>
         <h1>Shipping Datas</h1>
@@ -251,6 +257,8 @@ const checkoutGenerator = () => {
             </div>`;
         return false;
     })
+}
+
 });
 
 }
